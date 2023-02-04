71d1f3de37d011ebecf90f9a186224dcbd4e0140
Kyle <kbburkholder@gmail.com>
2023-02-03 22:53:41 -0500

    set_center example

--- source/entities/square.lua
+++ source/entities/square.lua
@@ -4,7 +4,7 @@ class('Square').extends(playdate.graphics.sprite)
 function Square:init()
     Square.super.init(self) -- this is critical
     self.image = Graphics.image.new("assets/images/patterns/longcheckerhor"):scaledImage(8)
-    
+    self:setCenter(0.0, 0.0)
     self:setImage(self.image)
     self:moveTo(100, 100)
     self:add()

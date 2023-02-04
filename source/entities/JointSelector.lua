8db9d41ddfd2928418d594bd920ad32e75045734
Kyle <kbburkholder@gmail.com>
2023-02-04 17:37:11 -0500

    movement

--- source/entities/JointSelector.lua
+++ source/entities/JointSelector.lua
@@ -17,57 +17,53 @@ function JointSelector:init(torso, head, hip, leftShoulder, leftArm, rightShould
     self.hip = JointWrapper(hip, self.torso, nil, nil, nil)
     self.leftArm = {
         JointWrapper(leftShoulder, nil, nil, nil, self.torso),
-        JointWrapper(leftArm, nil, nil, nil, nil)
+        JointWrapper(leftArm, nil, self.hip, nil, nil)
     }
     self.leftArm[1]:setLeftJoint(self.leftArm[2])
     self.leftArm[1]:setDownJoint(self.leftArm[2])
     
     self.leftArm[2]:setUpJoint(self.leftArm[1])
-    self.leftArm[2]:setRightJoint(self.torso)
+    self.leftArm[2]:setRightJoint(self.leftArm[1])
     self.leftArm[1]:setRightJoint(self.torso)
 
     self.leftLeg = {
-        JointWrapper(leftUpperLeg, self.leftArm[2], nil, nil, nil),
+        JointWrapper(leftUpperLeg, self.hip, nil, nil, nil),
         JointWrapper(leftLowerLeg, nil, nil, nil, nil)
     }
     self.leftLeg[1]:setDownJoint(self.leftLeg[2])
+    self.leftLeg[1]:setLeftJoint(self.leftLeg[2])
     self.leftLeg[2]:setUpJoint(self.leftLeg[1])
-    self.leftLeg[2]:setDownJoint(self.leftArm[1])
-    self.leftArm[1]:setUpJoint(self.leftLeg[2])
-    self.leftArm[2]:setDownJoint(self.leftLeg[1])
-    self.leftLeg[2]:setDownJoint(self.leftArm[1])
+    self.leftArm[1]:setUpJoint(self.torso)
     
     self.rightArm = {
         JointWrapper(rightShoulder, nil, nil, self.torso, nil),
-        JointWrapper(rightArm, nil, nil, nil, nil)
+        JointWrapper(rightArm, nil, self.hip, nil, nil)
     }
     self.rightArm[1]:setRightJoint(self.rightArm[2])
     self.rightArm[1]:setDownJoint(self.rightArm[2])
     self.rightArm[2]:setLeftJoint(self.rightArm[1])
     self.rightArm[2]:setUpJoint(self.rightArm[1])
-    self.leftArm[2]:setLeftJoint(self.rightArm[2])
     
 
     self.rightLeg = {
-        JointWrapper(rightUpperLeg, self.rightArm[2], nil, nil, self.hip),
-        JointWrapper(rightLowerLeg, nil, self.rightArm[1], nil, nil)
+        JointWrapper(rightUpperLeg, self.hip, nil, nil, self.hip),
+        JointWrapper(rightLowerLeg, nil, nil, nil, nil)
     }
     self.rightLeg[1]:setDownJoint(self.rightLeg[2])
+    self.rightLeg[1]:setRightJoint(self.rightLeg[2])
+    self.rightLeg[1]:setLeftJoint(self.hip)
     self.rightLeg[2]:setUpJoint(self.rightLeg[1])
-    self.rightLeg[2]:setRightJoint(self.leftLeg[2])
-    self.rightArm[2]:setRightJoint(self.rightLeg[1])
-    self.rightArm[2]:setDownJoint(self.rightLeg[1])
-    self.rightArm[1]:setUpJoint(self.rightLeg[2])
-    self.leftLeg[2]:setLeftJoint(self.rightLeg[2])
-    self.rightLeg[2]:setLeftJoint(self.leftLeg[2])
-    self.leftLeg[2]:setRightJoint(self.rightLeg[2])
+    self.rightLeg[2]:setLeftJoint(self.rightLeg[1])
+    self.rightArm[1]:setUpJoint(self.torso)
+    self.leftLeg[2]:setRightJoint(self.leftLeg[1])
+    self.leftLeg[1]:setRightJoint(self.hip)
     self.leftLeg[1]:setRightJoint(self.hip)
 
     
     self.hip:setLeftJoint(self.leftLeg[1])
     self.hip:setRightJoint(self.rightLeg[1])
-    self.hip:setDownJoint(self.torso)
-    self.head:setUpJoint(self.hip)
+
+
 
     self.torso:setUpJoint(self.head)
     self.torso:setDownJoint(self.hip)

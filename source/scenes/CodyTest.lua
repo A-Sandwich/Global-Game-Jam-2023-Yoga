ed0f1b1787bca0c9a6b38b9635950e5d8cabdd59
Kyle <kbburkholder@gmail.com>
2023-02-04 21:18:52 -0500

    fixed it

--- source/scenes/CodyTest.lua
+++ source/scenes/CodyTest.lua
@@ -157,7 +157,7 @@ function CodyTest:enter()
     llLegJoint = Joint(0, 0, 0, 40, luLegJoint, 90, lLLegSprite)
     ruLegJoint = Joint(0, 0, 0, 40, lBodyJoint, 135, rULegSprite)
     rlLegJoint = Joint(0, 0, 0, 40, ruLegJoint, 90, rLLegSprite)
-    confirm = Joint(0, 0, 0, 16, headJoint, 270, confirmSprite)
+    confirm = Joint(200, 50, 0, 16, nil, 0, confirmSprite)
 
     joints[1] = uBodyJoint
     joints[2] = headJoint

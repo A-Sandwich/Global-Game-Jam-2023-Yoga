16e1b0bb61801115620a35d5744e5037146bcea4
galwinganoon <galwinganoon@yahoo.com>
2023-02-05 10:17:54 -0500

    increased sparkle size.  fixed issue with last frame not showing

--- source/entities/Sparkle.lua
+++ source/entities/Sparkle.lua
@@ -7,7 +7,7 @@ end
 function Sparkle:init(onAnimFinish, key)
     self.imgTable = playdate.graphics.imagetable.new("assets/images/sparkle-table-7-7")
     Sparkle.super.init(self, self.imgTable) -- this is critical
-    self:addState('idle', 1, 4, { tickStep = 6, loop = false, onLoopFinishedEvent = onFinishCallback })
+    self:addState('idle', 1, 5, { tickStep = 6, loop = false, onLoopFinishedEvent = onFinishCallback, xScale = 2, yScale = 2 })
     self:playAnimation()
     self.onAnimFinish = onAnimFinish
     self.key = key
